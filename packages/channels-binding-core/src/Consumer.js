d2a365e6ad89038f7a2d0aadd21ca402d86d8d3c
dalou <autrusseau.damien@gmail.com>
2020-10-15 20:00:59 +0000

    "Package fixing"

--- packages/channels-binding-core/src/Consumer.js
+++ packages/channels-binding-core/src/Consumer.js
@@ -2,16 +2,230 @@ import REWebSocket, { globalParams } from './REWebSocket'
 
 const registry = {}
 const consumers = {
-    register: (name, path, onStateUpdate) => {
+    registry: registry,
+    register: (name, options, onStateUpdate) => {
 
-        console.log(path)
-        const url = new URL(path)
-        console.log(url)
-        const consumer = new Consumer({
-            host: url.hostname,
-            port: url.port,
-            path: url.pathname,
-            onStateUpdate
+        if (!_.isObject(options)) {
+
+            const url = new URL(path)
+            options = {
+                host: url.hostname,
+                port: url.port,
+                path: url.pathname,
+                onStateUpdate
+            }
+        }
+
+        this.host = host || window.location.hostname
+        this.port = port || window.location.port
+        this.protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
+        const debug = options.debug || 0
+        const url = new URL(`${this.protocol}//${this.host}:${this.port}`)
+        url.pathname = path || ''
+        // this.onStateUpdate = onStateUpdate || (() => { })
+
+        const consumer = new (class extends Consumer {
+
+            url = url
+            active = false
+            connected = false
+            pending = false
+            pending_calls = {}
+            listeners = {}
+            unique_listeners = {}
+            socket = null
+            debug = 0
+
+            constructor(...args) {
+                try {
+                    super(...args)
+                }
+                catch (err) {
+                    console.log(err)
+                    throw (`
+Illegal constructor instructions for ${ns} API listener.
+[send/on/once] should\'nt be called at invocation.
+Because of the internal automated API crossing dependency system it's recommanded to use hook effects or mounting methods.
+Try something like this on the main component that requires crossing API features: 
+
+    React.useEffect( () => 
+    { 
+        API.yourapi.connected && 
+            API.yourapi.once('receive_data') &&
+            API.yourapi.send('ask_for_data')
+        
+        API.anotherapi.on('logged_off', API.yourapi.close())
+    }, [ 
+        API.anotherapi.authenticated && API.yourapi.connected
+    ]) 
+`)
+                }
+            }
+
+            connect() {
+                if (this.active) return;
+                this.active = true;
+                this.pending = true;
+                this.debug && this.logSuccess('Connecting', this.url.href)
+                this.socket = new REWebSocket(this.url)
+                this.socket.addEventListener('open', e => this.onOpen());
+                this.socket.addEventListener('close', e => this.onClose());
+                this.socket.addEventListener('message', e => this.receive());
+                this.updateState()
+            }
+
+            onOpen() {
+                console.log('open', this)
+                this.pending = false;
+                this.connected = true;
+                this.debug && this.logSuccess('Connected')
+                _.map(this.pending_calls, (data, event) => {
+                    delete this.pending_calls[event]
+                    this.send(event, data)
+                })
+                this.updateState()
+            }
+
+            onClose(message) {
+                this.pending = false;
+                this.connected = false
+                this.debug && this.logError('Closed')
+                this.updateState()
+            }
+
+            close() {
+                this.socket && (this.socket.close())
+                this.pending = false;
+                this.connected = false;
+                this.updateState()
+            }
+
+            send(event, data, delay) {
+                this.connect()
+                if (this.connected) {
+                    this.debug && this.logInfo('Sent', event, data);
+                    if (delay) {
+                        setTimeout(() => (
+                            this.socket.send(JSON.stringify({ event, data }))
+                        ), delay);
+                    }
+                    else {
+                        this.socket.send(JSON.stringify({ event, data }));
+                    }
+                }
+                else {
+                    this.pending_calls[event] = data
+                    this.debug && this.logError('Not sent', 'Socket is not connected yet')
+                }
+            }
+
+
+            updateState() {
+                const { active, connected, pending } = this
+                this.onStateUpdate && this.onStateUpdate({
+                    active, connected, pending
+                })
+            }
+
+            setGlobalParams(name, value) {
+                globalParams[name] = value
+            }
+
+            delGlobalParams(name, value) {
+                globalParams[name] && (delete globalParams[name])
+            }
+
+            useEventEffect(event, method, listen, condition = true) {
+                const [fetching, setFetching] = React.useState(condition)
+                React.useEffect(() => this.attachListener(event, method, condition, setFetching), listen || [])
+                return fetching
+            }
+
+            attachListener(event, method, condition = true, setFetching) {
+                const handler = (...args) => {
+                    method && method(...args)
+                    setFetching && setFetching(false)
+                }
+                const disposer = this.addListener(event, handler)
+                condition && this.send(event, options) && setFetching && setFetching(true)
+                return disposer
+            }
+
+
+            addListener(event, method) {
+                this.debug && this.debug > 1 && this.logInfo(`addListener: ${event}.${method && method.name}`);
+                this.connect()
+                !this.listeners[event] && (this.listeners[event] = {})
+                // Replace the method ref pointer
+                this.listeners[event][method] = method
+                return () => {
+                    this.removeListener(event, method)
+                }
+            }
+
+            removeListener(event, method) {
+                this.debug && this.debug > 1 && this.logInfo(`removeListener: ${event}.${method && method.name}`);
+                try { delete this.listeners[event][method] }
+                catch (err) { this.debug && this.debug > 1 && this.logError(`removeListener: ${event}.${method && method.name} not found.`) }
+            }
+
+            once(event, method) {
+                this.debug && this.logInfo(`once: Method ${event}.${method && method.name}`);
+                this.connect()
+                this.unique_listeners[event] = method
+            }
+
+
+            receive(message) {
+                const data = JSON.parse(message.data)
+                if (data.event == 'error') {
+                    this.debug && this.logError('Error', data.data)
+                }
+                else if (data.event) {
+                    this.debug && this.logSuccess('Receive', data.event)
+                    _.has(this.listeners, data.event) &&
+                        _.map(this.listeners[data.event], method => method && method(data.data, this))
+
+                    if (_.has(this.unique_listeners, data.event)) {
+                        this.unique_listeners[data.event](data.data, this)
+                        delete this.unique_listeners[data.event]
+                    }
+                }
+            }
+
+            request(path, callback, options) {
+                options = _.defaults(options, { method: 'GET' })
+                this.debug && this.logInfo(options.method, event, options);
+                fetch(this.link(path), options)
+                    .then(r => r.json())
+                    .then(callback)
+                    .catch(error => {
+                        this.logError(options.method, error);
+                    });
+            }
+
+            link(path) {
+                const url = new URL(this.url)
+                url.pathname = url.pathname + path
+                url.protocol = document.location.protocol
+                return url.href
+            }
+
+            log(message, color, ...messages) {
+                console.log(`%c [${message}] %c `, `font-weight:bolder; ${color}`, 'font-weight:bold', ...messages)
+            }
+
+            logSuccess(message, ...messages) {
+                this.log(message, 'background: #bada55; color: #222', ...messages)
+            }
+
+            logInfo(message, ...messages) {
+                this.log(message, 'background: #222; color: #bada55', ...messages)
+            }
+
+            logError(message, ...messages) {
+                this.log(message, 'background: red; color: white', ...messages)
+            }
         })
         registry[name] = consumer
     }
@@ -20,187 +234,7 @@ const consumers = {
 
 class Consumer {
 
-    active = false
-    connected = false
-    pending = false
-    pending_calls = {}
-    listeners = {}
-    unique_listeners = {}
-    socket = null
-    debug = 0
 
-    constructor({ host, port, path, onStateUpdate }) {
-
-        this.host = host || window.location.hostname
-        this.port = port || window.location.port
-        this.protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
-        this.url = new URL(`${this.protocol}//${this.host}:${this.port}`)
-        this.url.pathname = path || ''
-        this.onStateUpdate = onStateUpdate
-        this.connect()
-    }
-
-    updateState() {
-        const { active, connected, pending } = this
-        this.onStateUpdate && this.onStateUpdate({
-            active, connected, pending
-        })
-    }
-
-    setGlobalParams(name, value) {
-        globalParams[name] = value
-    }
-
-    delGlobalParams(name, value) {
-        globalParams[name] && (delete globalParams[name])
-    }
-
-    useEventEffect(event, method, listen, condition = true) {
-        const [fetching, setFetching] = React.useState(condition)
-        React.useEffect(() => this.attachListener(event, method, condition, setFetching), listen || [])
-        return fetching
-    }
-
-    attachListener(event, method, condition = true, setFetching) {
-        const handler = (...args) => {
-            method && method(...args)
-            setFetching && setFetching(false)
-        }
-        const disposer = this.addListener(event, handler)
-        condition && this.send(event, options) && setFetching && setFetching(true)
-        return disposer
-    }
-
-
-    addListener(event, method) {
-        this.debug && this.debug > 1 && this.logInfo(`addListener: ${event}.${method && method.name}`);
-        this.connect()
-        !this.listeners[event] && (this.listeners[event] = {})
-        // Replace the method ref pointer
-        this.listeners[event][method] = method
-        return () => {
-            this.removeListener(event, method)
-        }
-    }
-
-    removeListener(event, method) {
-        this.debug && this.debug > 1 && this.logInfo(`removeListener: ${event}.${method && method.name}`);
-        try { delete this.listeners[event][method] }
-        catch (err) { this.debug && this.debug > 1 && this.logError(`removeListener: ${event}.${method && method.name} not found.`) }
-    }
-
-    once(event, method) {
-        this.debug && this.logInfo(`once: Method ${event}.${method && method.name}`);
-        this.connect()
-        this.unique_listeners[event] = method
-    }
-
-    send(event, data, delay) {
-        this.connect()
-        if (this.connected) {
-            this.debug && this.logInfo('Sent', event, data);
-            if (delay) {
-                setTimeout(() => (
-                    this.socket.send(JSON.stringify({ event, data }))
-                ), delay);
-            }
-            else {
-                this.socket.send(JSON.stringify({ event, data }));
-            }
-        }
-        else {
-            this.pending_calls[event] = data
-            this.debug && this.logError('Not sent', 'Socket is not connected yet')
-        }
-    }
-
-    receive(message) {
-        const data = JSON.parse(message.data)
-        if (data.event == 'error') {
-            this.debug && this.logError('Error', data.data)
-        }
-        else if (data.event) {
-            this.debug && this.logSuccess('Receive', data.event)
-            _.has(this.listeners, data.event) &&
-                _.map(this.listeners[data.event], method => method && method(data.data, this))
-
-            if (_.has(this.unique_listeners, data.event)) {
-                this.unique_listeners[data.event](data.data, this)
-                delete this.unique_listeners[data.event]
-            }
-        }
-    }
-
-    request(path, callback, options) {
-        options = _.defaults(options, { method: 'GET' })
-        this.debug && this.logInfo(options.method, event, options);
-        fetch(this.link(path), options)
-            .then(r => r.json())
-            .then(callback)
-            .catch(error => {
-                this.logError(options.method, error);
-            });
-    }
-
-    link(path) {
-        const url = new URL(this.url)
-        url.pathname = url.pathname + path
-        url.protocol = document.location.protocol
-        return url.href
-    }
-
-    connect() {
-        if (this.active) return;
-        this.active = true;
-        this.pending = true;
-        this.debug && this.logSuccess('Connecting', this.url.href)
-        this.socket = new REWebSocket(this.url)
-        this.socket.addEventListener('open', this.onOpen);
-        this.socket.addEventListener('close', this.onClose);
-        this.socket.addEventListener('message', this.receive);
-        this.updateState()
-    }
-
-    onOpen() {
-        this.pending = false;
-        this.connected = true;
-        this.debug && this.logSuccess('Connected')
-        _.map(this.pending_calls, (data, event) => {
-            delete this.pending_calls[event]
-            this.send(event, data)
-        })
-        this.updateState()
-    }
-
-    onClose(message) {
-        this.pending = false;
-        this.connected = false
-        this.debug && this.logError('Closed')
-        this.updateState()
-    }
-
-    close() {
-        this.socket && (this.socket.close())
-        this.pending = false;
-        this.connected = false;
-        this.updateState()
-    }
-
-    log(message, color, ...messages) {
-        console.log(`%c [${message}] %c `, `font-weight:bolder; ${color}`, 'font-weight:bold', ...messages)
-    }
-
-    logSuccess(message, ...messages) {
-        this.log(message, 'background: #bada55; color: #222', ...messages)
-    }
-
-    logInfo(message, ...messages) {
-        this.log(message, 'background: #222; color: #bada55', ...messages)
-    }
-
-    logError(message, ...messages) {
-        this.log(message, 'background: red; color: white', ...messages)
-    }
 
 }
 
--- packages/channels-binding-core/src/index.js
+++ packages/channels-binding-core/src/index.js
@@ -1,24 +1,69 @@
-export { default as Bind } from './binds/Bind'
-export { default as useBind } from './binds/useBind'
+// export { default as Bind } from './binds/Bind'
+// export { default as useBind } from './binds/useBind'
 
-export { default as Search } from './binds/Search'
-export { default as useSearch } from './binds/useSearch'
+// export { default as Search } from './binds/Search'
+// export { default as useSearch } from './binds/useSearch'
 
-export { default as Retrieve } from './binds/Retrieve'
-export { default as useRetrieve } from './binds/useRetrieve'
+// export { default as Retrieve } from './binds/Retrieve'
+// export { default as useRetrieve } from './binds/useRetrieve'
 
-export { default as Form } from './binds/Form'
-export { default as useForm } from './binds/useForm'
+// export { default as Form } from './binds/Form'
+// export { default as useForm } from './binds/useForm'
 
-export { default as Save } from './binds/Save'
-export { default as useSave } from './binds/useSave'
+// export { default as Save } from './binds/Save'
+// export { default as useSave } from './binds/useSave'
 
-export { default as Delete } from './binds/Delete'
-export { default as useDelete } from './binds/useDelete'
+// export { default as Delete } from './binds/Delete'
+// export { default as useDelete } from './binds/useDelete'
 
-export { default as REWebSocket } from './REWebSocket'
+// export { default as REWebSocket } from './REWebSocket'
 
-export { default as useDebouncedState } from './utils/useDebouncedState'
-export { default as useDebounced } from './utils/useDebounced'
+// export { default as useDebouncedState } from './utils/useDebouncedState'
+// export { default as useDebounced } from './utils/useDebounced'
 
-export { consumers } from './Consumer'
+// export { consumers } from './Consumer'
+
+import Bind from './binds/Bind'
+import useBind from './binds/useBind'
+
+import Search from './binds/Search'
+import useSearch from './binds/useSearch'
+
+import Retrieve from './binds/Retrieve'
+import useRetrieve from './binds/useRetrieve'
+
+import Form from './binds/Form'
+import useForm from './binds/useForm'
+
+import Save from './binds/Save'
+import useSave from './binds/useSave'
+
+import Delete from './binds/Delete'
+import useDelete from './binds/useDelete'
+
+import REWebSocket from './REWebSocket'
+
+import useDebouncedState from './utils/useDebouncedState'
+import useDebounced from './utils/useDebounced'
+
+import { consumers } from './Consumer'
+
+export {
+    Bind,
+    useBind,
+    Search,
+    useSearch,
+    Retrieve,
+    useRetrieve,
+    Form,
+    useForm,
+    Save,
+    useSave,
+    Delete,
+    useDelete,
+    REWebSocket,
+    useDebouncedState,
+    useDebounced,
+    consumers
+}
+
